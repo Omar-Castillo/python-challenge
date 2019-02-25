90b7abe28caab6b36ab9cc85d98edd4be76c7c0b
Omar Castillo <omar.castillo09@gmail.com>
2019-02-25 15:28:20 -0600

    Update main.py
    calculated the average change

--- Pybank/main.py
+++ Pybank/main.py
@@ -3,10 +3,13 @@ import os
 #module for reading csv files
 import csv
 
+#since file is in the same folder as our main.py, I only need to designate file name
 csvpath = os.path.join('budget_data.csv')
 
+#define variables we will be working  with
 months = []
 profitloss = []
+change_list= []
 
 #method for reading of csv file
 
@@ -16,15 +19,34 @@ with open(csvpath, newline='') as csv_file:
     for lines in csv_reader:
         months.append(lines[0])
         profitloss.append(lines[1])
+       # print(profitloss)
 
+
+    total_months = len(months[1:])
     
-    total_months = len(months)
-    total_profitloss = sum(int(profitloss))
-    print (total_months)
-    print (total_profitloss)
+    total_profitloss = sum(list(map(int,profitloss[1:])))
 
+    int_profitloss = list(map(int,profitloss[1:]))
 
+    # use a loop to go through the profitloss list and calculate the differences into a new list called changes
+    for n in range(1,len(int_profitloss)):
+        changes = (int_profitloss[n] - int_profitloss[n-1])
+        change_list.append(changes)
     
+    #print(change_list)
+    #calculate the change average, below we sum up the change list, and divide by the length(elements) in the list, we then use the round function to keep 2 decimal places
+    average_changes = round(((sum(change_list)) / (len(change_list))),2)
+
+    print(f'Financial Analysis')
+    print(f'------------------------------------------------------------------')
+    print (f'Total Months: {total_months}')
+    print (f'Total: ${total_profitloss}')
+
+    #print(int_profitloss)
+    print(f'Average Changes: ${average_changes}')
+
+   # print(profitloss[1])
+
     #csv_header = next(csv_reader)
     #print (f'CSV Header: {csv_header}')
 
